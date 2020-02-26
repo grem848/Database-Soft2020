c53495d5efc6865b207227263c7d0a01c050a508
Andreas <andreas848@hotmail.com>
2020-02-26 15:05:09 +0100

    trigger function cancellationcheck24h plus trigger, create full booking

--- Mini-project 1/HotelDB_SqlFunctions.sql
+++ Mini-project 1/HotelDB_SqlFunctions.sql
@@ -154,3 +154,68 @@ END; $$
 LANGUAGE 'plpgsql';
 
 -- Run --> SELECT * FROM getNumberOfBookingsInMonth(date '2020-03-01', date '2020-03-01')
+
+
+
+
+/* TRIGGER FUNCTION TO AVOID CANCELLATION WITHIN 24 HOURS OF BOOKING START */
+CREATE OR REPLACE FUNCTION cancellationCheck24H() 
+	RETURNS TRIGGER AS 
+$BODY$ 
+DECLARE
+start_date TIMESTAMP;
+compare_date TIMESTAMP;
+BEGIN  
+	IF new.is_cancelled IS TRUE THEN
+		start_date := (select starts from public."Booked_Rooms" where booking_id = old.id);
+		RAISE NOTICE 'start_date: %', start_date;
+		compare_date := (now() + interval '24 hours');
+		RAISE NOTICE 'compare_date: %', compare_date;
+		
+		IF start_date < compare_date THEN
+			RAISE EXCEPTION 'CANNOT CANCEL WITHIN 24 HOURS OF BOOKING START!';
+		ELSE
+			RETURN NEW;
+		END IF;
+	END IF;
+	RETURN NEW;
+END;  
+$BODY$ 
+LANGUAGE plpgsql;  
+
+/* TRIGGER FOR TABLE BOOKINGS */
+DROP TRIGGER cancellationcheck24h ON public."Bookings";
+CREATE TRIGGER cancellationcheck24h
+    BEFORE UPDATE OF is_cancelled
+    ON public."Bookings"
+    FOR EACH ROW
+    EXECUTE PROCEDURE public.cancellationcheck24h();
+	
+	
+/* CREATE FULL BOOKING */
+DO $$
+	DECLARE
+   		bookings integer := (SELECT count(*) 
+					 FROM public."Booked_Rooms" 
+					 WHERE room_number = 8  
+					 AND starts >= timestamp '2020-03-22 10:00:00'  
+					 AND ends <= timestamp '2020-03-23 10:00:00');
+		timeNow TIMESTAMP := now();
+		booking_id integer;
+BEGIN
+	IF bookings > 0 THEN
+		RAISE EXCEPTION 'ALREADY BOOKED';
+	ELSE 
+		INSERT INTO "Bookings" (primary_guest_id, guests_amount, booking_created, is_group_event, is_cancelled)  VALUES ( 1, 20, timeNow, TRUE , FALSE);
+		booking_id := (SELECT id FROM "Bookings" WHERE primary_guest_id = 1 AND guests_amount = 20 AND booking_created = timeNow AND is_group_event = TRUE AND is_cancelled =  FALSE);
+		RAISE WARNING 'booking_id is %', booking_id;
+	END IF;
+	INSERT INTO "Guest_bookings" ( guest_id, booking_id) VALUES ( 1, booking_id );
+	INSERT INTO "Booked_Rooms" ( booking_id, room_number, starts, ends ) VALUES (booking_id, 8 , '2020-03-22 10:00:00', '2020-03-23 10:00:00' );
+	EXCEPTION 
+		WHEN OTHERS THEN 
+		RAISE WARNING 'EXCEPTION CALLED ';
+		ROLLBACK;
+	RAISE NOTICE 'BOOKING SUCCESSFUL';
+	COMMIT;
+END $$;
